43fe7059d9dea55ae16417f02d91828348040314
sumit298 <sumitsinhassss@outlook.com>
2021-10-22 23:13:54 +0530

    Added Bottom Navigator for Home History and Analytics

--- android/app/src/main/java/com/mood_tracker/MainActivity.java
+++ android/app/src/main/java/com/mood_tracker/MainActivity.java
@@ -1,8 +1,17 @@
 package com.mood_tracker;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  /* 
+    * React Native Screen requires one additional configuration step to properly work on Android devices
+  
+  */
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
